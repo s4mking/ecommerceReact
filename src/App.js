6e7f47b4c2fb009f4da021a2f9057c511ef10f2f
s4mking <samuel.simonney@gmail.com>
2020-02-12 11:49:49 +0100

    Change to products preview

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
-import HomePage from './components/homepage/homepage.component'
+import HomePage from './pages/homepage/homepage.component'
+import shopPage from './pages/shop/shop.component'
+import { Route, Switch } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-        <HomePage/>
+    <div>
+      <Switch>
+      <Route exact path='/' component={HomePage}/>
+      <Route exact path='/shop' component={shopPage}/>
+      </Switch>
     </div>
   );
 }
